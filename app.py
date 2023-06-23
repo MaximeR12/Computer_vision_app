6d213bc032237f847c7155fea68a4348a8d85091
MaximeR12 <rowell.maxime2001@gmail.com>
2023-06-23 14:10:34 +0200

    initial commit MVP

--- app.py
+++ app.py
@@ -13,12 +13,11 @@ from PIL import Image
 
 st.title("Soda brand detector")
 st.write("By Emmanuel & Maxime")
-uploaded_files = st.file_uploader("Choisissez un fichier", type=["png", "jpeg", "jpg"], accept_multiple_files=True)
+uploaded_files = st.file_uploader("Entrez un image", type=["png", "jpeg", "jpg"], accept_multiple_files=True)
 if uploaded_files :
     for file in uploaded_files:         # Boucle for pour ouvrir toutes les images si on en a plusieurs
-        image_base = Image.open(file)   # J'utilise la librairie PIL pour transformer l'image
+        image_base = Image.open(file)   # Utilisation la librairie PIL pour transformer l'image
         st.image(image_base)            # L'image non moidifiée/prédite s'affiche
         result = model(image_base)      # Prediction effectuée sur l'image
         st.image(result[0].plot())      # la prediction est une liste mais la methode plot sert a retransformer en image
-        st.write(result)          # Boxes object for bbox outputs)
 
