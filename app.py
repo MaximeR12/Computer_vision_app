f05a376b221ce4af3f38853ed1681eb2d100a386
MaximeR12 <rowell.maxime2001@gmail.com>
2023-06-23 12:29:39 +0200

    MVP, model needs improvement

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 ############################
 from ultralytics import YOLO
 
-model = YOLO("models/bestl.pt")
+model = YOLO("models/best_v2.pt")
 
 ############################
 #      Streamlit app       #
@@ -11,13 +11,14 @@ model = YOLO("models/bestl.pt")
 import streamlit as st
 from PIL import Image
 
-st.title("Application Streamlit avec File Uploader")
+st.title("Soda brand detector")
+st.write("By Emmanuel & Maxime")
 uploaded_files = st.file_uploader("Choisissez un fichier", type=["png", "jpeg", "jpg"], accept_multiple_files=True)
 if uploaded_files :
-    for file in uploaded_files:    # Boucle for pour ouvrir toutes les images si on en a plusieurs
+    for file in uploaded_files:         # Boucle for pour ouvrir toutes les images si on en a plusieurs
         image_base = Image.open(file)   # J'utilise la librairie PIL pour transformer l'image
         st.image(image_base)            # L'image non moidifiée/prédite s'affiche
         result = model(image_base)      # Prediction effectuée sur l'image
         st.image(result[0].plot())      # la prediction est une liste mais la methode plot sert a retransformer en image
-
+        st.write(result)          # Boxes object for bbox outputs)
 
